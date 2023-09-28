7346e8c01c91257a27859935ea2ed006ecbe08d7
Vinay C N <vinaycngowda@gmail.com>
2023-09-28 14:15:20 +0530

    Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args){
+    RWOnly rw  = new RWOnly();
+    rw.setter("vinay");
+    System.out.println(rw.getter());
+    //System.out.println(rw.name); //The field RWOnly.name is not visible
+  }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,23 @@
 package com.driver;
 
-public class RWOnly {
-    
+public  class RWOnly {
+ private String name;
+  
+void RWOnly(String name){
+    this.name = name;
 }
+ void setter(String name){
+    this.name = name;
+ }
+
+ public String getter(){
+    return name;
+ }
+
+
+
+}
+
+
+   
+    
